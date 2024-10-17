41c6a64e778cae2e6fa714c4a20b2ea9760d4e9f
Vaibhavvi <163631801+Vaibhavvi@users.noreply.github.com>
2024-10-17 22:33:41 +0530

    index.html

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ export default class App extends Component {
       <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route caseSensitive path='/health' element={<News key='home' pageSize={5} country="us" category="health"/>}></Route>
-        <Route caseSensitive path='/' element={<News key='helth' pageSize={5} country="us" category="science"/>}></Route>
-        <Route caseSensitive path='/technology' element={<News key='technology' pageSize={5} country="us" category="technology"/>}></Route>
-        <Route caseSensitive path='/sport' element={<News key='sport' pageSize={5} country="us" category="sport"/>}></Route>
-        <Route caseSensitive path='/business' element={<News key='business' pageSize={5} country="us" category="business"/>}></Route>
-        <Route caseSensitive path='/science' element={<News key='science' pageSize={5} country="us" category="science"/>}></Route>
-        <Route caseSensitive path='/entertanment' element={<News key='entertanment' pageSize={5} country="us" category="entertanment"/>}></Route>
+        <Route caseSensitive path='/NewNews/health' element={<News key='home' pageSize={5} country="us" category="health"/>}></Route>
+        <Route caseSensitive path='/NewNews/' element={<News key='helth' pageSize={5} country="us" category="science"/>}></Route>
+        <Route caseSensitive path='/NewNews/technology' element={<News key='technology' pageSize={5} country="us" category="technology"/>}></Route>
+        <Route caseSensitive path='/NewNews/sport' element={<News key='sport' pageSize={5} country="us" category="sport"/>}></Route>
+        <Route caseSensitive path='/NewNews/business' element={<News key='business' pageSize={5} country="us" category="business"/>}></Route>
+        <Route caseSensitive path='/NewNews/science' element={<News key='science' pageSize={5} country="us" category="science"/>}></Route>
+        <Route caseSensitive path='/NewNews/entertanment' element={<News key='entertanment' pageSize={5} country="us" category="entertanment"/>}></Route>
         </Routes>  
       </BrowserRouter>
       </div>
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -8,29 +8,29 @@ export default class Navbar extends Component {
       <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">WorldNews</Link>
+            <Link className="navbar-brand" to="/NewNews/">WorldNews</Link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auhref mb-2 mb-lg-0">
               <li className="nav-item">
-                  <Link className="nav-link" to="/">Home</Link>
+                  <Link className="nav-link" to="/NewNews/">Home</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/technology">Technology</Link>
+                  <Link className="nav-link" to="/NewNews/technology">Technology</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/sport">Sport</Link>
+                  <Link className="nav-link" to="/NewNews/sport">Sport</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/business">Business</Link>
+                  <Link className="nav-link" to="/NewNews/business">Business</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/science">Science</Link>
+                  <Link className="nav-link" to="/NewNews/science">Science</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/entertanment">Entertanment</Link>
+                  <Link className="nav-link" to="/NewNews/entertanment">Entertanment</Link>
                 </li>
               </ul>
             </div>
