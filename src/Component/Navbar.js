7d48a928257c30fff648e34c23ef417549d22323
Vaibhavvi <163631801+Vaibhavvi@users.noreply.github.com>
2024-10-17 22:08:23 +0530

    index.html

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -8,7 +8,7 @@ export default class Navbar extends Component {
       <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/health">WorldNews</Link>
+            <Link className="navbar-brand" to="/">WorldNews</Link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
